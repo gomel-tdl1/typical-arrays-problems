7ae1a45afc28703c36d4aad6ac11250349d4a83a
Ilya Taldykin <rock_cell@mail.ru>
2020-09-29 17:38:47 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+exports.min = function min(array) {
+    if (!array||array.length===0) {
+        return 0;
+    }else{
+        return array.reduce((prev, current) => {
+            if (current < prev) {
+                return current;
+            } else {
+                return prev;
+            }
+        }, 1000000000000);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array||array.length===0) {
+        return 0;
+    }else{
+        return array.reduce((prev, current) => {
+            if (current > prev) {
+                return current;
+            } else {
+                return prev;
+            }
+        }, -10000000000);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array||array.length===0) {
+        return 0;
+    }else{
+        return array.reduce((prev, current) => {
+            return prev + current;
+        }, 0) / array.length;
+    }
+};
